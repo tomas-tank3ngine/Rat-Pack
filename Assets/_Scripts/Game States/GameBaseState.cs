9beac3b05ce31d6cdb287aeb5cd8f3cfc6852bfb
Tomas Martinez <117690704+tomas-tank3ngine@users.noreply.github.com>
2026-05-08 21:32:02 -0400

    Add Game States and Managers

--- Assets/_Scripts/Game States/GameBaseState.cs
+++ Assets/_Scripts/Game States/GameBaseState.cs
@@ -1,9 +1,7 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameBaseState : MonoBehaviour
 {
-    // Responsible for starting the 
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
