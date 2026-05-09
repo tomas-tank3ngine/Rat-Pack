76268dc46390a9da0c65acaf1d4a29c1a6614455
Tomas Martinez <117690704+tomas-tank3ngine@users.noreply.github.com>
2026-05-09 19:30:09 -0400

    Buttons to look at counter vs cheese cutting station
    Made buttons interactable/not depending on view
    Added smooth slerp rotation to camera views when switching

--- Assets/_Scripts/CameraManager.cs
+++ Assets/_Scripts/CameraManager.cs
@@ -1,19 +1,87 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class CameraManager : MonoBehaviour
 {
     public Camera Camera;
-    //public Vector3 cheeseCuttingTarget;
-    //public Vector3 counterTarget;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [Header("Camera Targets")]
+    public GameObject cheeseCuttingTarget;
+    public GameObject counterTarget;
+
+    [Header("UI Buttons")]
+    [SerializeField] private Button lookCounterButton;
+    [SerializeField] private List<Button> lookCheeseButtons;
+
+    [Header("Camera Movement")]
+    [SerializeField] private float rotationDuration = 0.15f;
+
+    private Coroutine activeRotation;
+
+    public void LookCounter()
     {
-        
+        RotateTo(counterTarget.transform.rotation);
+
+        // Enable all cheese buttons
+        foreach (Button button in lookCheeseButtons)
+        {
+            button.interactable = true;
+        }
+
+        // Disable counter button
+        lookCounterButton.interactable = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LookCheeseCut()
     {
-        
+        RotateTo(cheeseCuttingTarget.transform.rotation);
+
+        // Enable counter button
+        lookCounterButton.interactable = true;
+
+        // Disable all cheese buttons
+        foreach (Button button in lookCheeseButtons)
+        {
+            button.interactable = false;
+        }
+    }
+
+    private void RotateTo(Quaternion targetRotation)
+    {
+        if (activeRotation != null)
+        {
+            StopCoroutine(activeRotation);
+        }
+
+        activeRotation = StartCoroutine(
+            SmoothRotate(targetRotation)
+        );
+    }
+
+    private IEnumerator SmoothRotate(Quaternion targetRotation)
+    {
+        Quaternion startRotation = Camera.transform.rotation;
+
+        float elapsed = 0f;
+
+        while (elapsed < rotationDuration)
+        {
+            elapsed += Time.deltaTime;
+
+            float t = elapsed / rotationDuration;
+
+            Camera.transform.rotation =
+                Quaternion.Slerp(
+                    startRotation,
+                    targetRotation,
+                    t
+                );
+
+            yield return null;
+        }
+
+        Camera.transform.rotation = targetRotation;
     }
 }
